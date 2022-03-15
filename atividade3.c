6e3428b3c9d94af7f50a9f88f6cb7932eb64275a
arthurlins7 <79600669+arthurlins7@users.noreply.github.com>
2022-03-14 22:48:21 -0300

    Update atividade3.c

--- atividade3.c
+++ atividade3.c
@@ -1,49 +1,36 @@
-#include <stdio.h>    
-#include <pthread.h>
-#define NUM_THREADS 4
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void *funcao(void *args)
+int main(int argc, char *argv[])
 {
-  int id, num = 0, total = 0, cont = 0, quant = 0;
+  int cont = 0;
+  int quant = 0, num = 0, num2 = 0, total = 0;
   int max = 0, min = 0, avg = 0;
-  id = *(int *)args;
-  if(id == 1){
-    scanf("%d", &quant);
-    do{
-      scanf("%d", &num);
-      total += num;
-      if(cont == 0){
-        min = num;
-      }
-      if(cont == 0){
-      
-      }
-      if(num > max){
-      max = num;
-      }
-      
-      else if(num < min){
+
+  scanf("%d", &quant);
+  
+  do{
+    scanf("%d", &num);
+    total += num;
+    if(cont == 0){
       min = num;
-      }
-      
-      cont++;
-    }while(cont < quant);
-  avg = total/quant;
-  printf("The average value is %d\n", avg);
-  printf("The minimum value is %d\n", min);
-  printf("The maximum value is %d\n", max);
-  }
-    return (NULL);
-  }
+    }
+    if(num > max){
+      max = num;
+    }
  
-int main ()
-{
-  pthread_t threads[NUM_THREADS];
-  int i;
-  for(i=0;i< NUM_THREADS;i++)
-       pthread_create(&threads[i], NULL, funcao, &i);
- 
-  for(i=0;i< NUM_THREADS;i++)
-      pthread_join(threads[i],NULL);
-  return 0;   
+    else if(num < min){
+      min = num;
+    }
+    
+    cont++;
+  }while(cont < quant);
+
+avg = total/quant;
+
+printf("The average value is %d\n", avg);
+printf("The minimum value is %d\n", min);
+printf("The maximum value is %d\n", max);
+  return 0;
 }
